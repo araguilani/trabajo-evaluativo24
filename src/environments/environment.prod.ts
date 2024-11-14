deb2a107e8f219cca4defdce1d118b460a05404c
araguilani <henriquezgianella41@gmail.com>
2024-11-14 08:44:58 -0300

    arregle la BD

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+// src/environments/environment.prod.ts
+
+export const environment = {
+    production: true
+  };
+  
+
+
+
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAkQtI-NQ2sMWcpUUEUHXD-VuRS9I2k8fk",
+  authDomain: "proyecto-2024-b1b86.firebaseapp.com",
+  projectId: "proyecto-2024-b1b86",
+  storageBucket: "proyecto-2024-b1b86.firebasestorage.app",
+  messagingSenderId: "24993211126",
+  appId: "1:24993211126:web:586a5be3d636b39d8f82d7"
+  }
+};
